fb5cb603342e116fc0e85f74402d257fdd97a705
Leslie <Kooliesk@gmail.com>
2023-08-13 14:48:52 +0200

    follow naming conventions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Greetings from './routes/Greetings';
+import Greetings from './routes/Greeting';
 
 const App = () => (
   <Router>
